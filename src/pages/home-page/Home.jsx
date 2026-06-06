c99fce65603bb91511ad8c991dc67e975e38687f
ahmed-mahmoud-goda <ahmed-goda@hotmail.com>
2026-06-06 21:00:37 +0300

    Added server proxy config for /api and /images

--- src/pages/home-page/Home.jsx
+++ src/pages/home-page/Home.jsx
@@ -6,18 +6,24 @@ import './Home.css'
 function Home() {
 
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
-        axios.get("http://localhost:3000/api/products")
+        axios.get("/api/products")
             .then((res) => {
                 setProducts(res.data);
+            })
+
+        axios.get("/api/cart-items")
+            .then((res) => {
+                setCart(res.data);
             })
     }, [])
 
 
     return (
         <>
-            <Header />
+            <Header cart={cart}/>
             <div className="home-page">
                 <div className="products-grid">
                     {products.map((product) => {
