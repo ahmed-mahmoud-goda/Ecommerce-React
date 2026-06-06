096d0d1ea74c764668f2bcf7949466e6b5c5cb69
ahmed-mahmoud-goda <ahmed-goda@hotmail.com>
2026-06-06 23:39:30 +0300

    finished fetching data from backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,28 @@ import './App.css'
 import Checkout from './pages/checkout-page/Checkout'
 import Orders from './pages/orders-page/Orders'
 import Tracking from './pages/tracking-page/tracking'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
 
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const getCartData = async () => {
+      const res = await axios.get("/api/cart-items?expand=product");
+      setCart(res.data);
+    }
+    getCartData();
+  }, [])
+
   return (
-      <Routes>
-        <Route index element={<Home/>}/>
-        <Route path='checkout' element={<Checkout/>}/>
-        <Route path='orders' element={<Orders/>}/>
-        <Route path='tracking' element={<Tracking/>}/>
-      </Routes>
+    <Routes>
+      <Route index element={<Home cart={cart} />} />
+      <Route path='checkout' element={<Checkout cart={cart} />} />
+      <Route path='orders' element={<Orders cart={cart} />} />
+      <Route path='tracking/:orderId/:productId' element={<Tracking cart={cart} />} />
+    </Routes>
   )
 }
 
